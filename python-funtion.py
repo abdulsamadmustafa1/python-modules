b05a3f61eeb555a0784cb19517afaaddbfc030a7
ladmin <ladmin@dell-5820.is.localnet>
2023-10-27 08:52:48 +0200

    updated from Linux Terminal

---
+++ python-funtion.py
@@ -0,0 +1,23 @@
+def hello():
+    print("Hello Python Function")
+    
+hello()
+
+
+# Traverse a Python list in reverse order
+# Using iterate list elements and 
+# insert() method
+
+list1 = [10, 20, 30, 40, 50]
+
+# Print the list
+print("Original list order: ", list1)
+
+# Create a new list with elements 
+# in reverse order
+newlist = [] # an empty list
+for item in list1:
+    newlist.insert(0,item)
+
+# Print the new list
+print("List in reverse order:",newlist)
